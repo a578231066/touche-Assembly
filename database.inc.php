a7b51c299e18f4a1353eb0932291f86f9240f2e3
a578231066 <578231066qq.com>
2018-01-10 12:05:57 +0800

    更新

---
+++ database.inc.php
@@ -0,0 +1,5 @@
+<?php
+return array(
+'default' => array (
+'hostname' => 'localhost','database' => 'yung','username' => 'root','password' => '7e8d85edf5','tablepre' => 'go_','charset' => 'utf8','type' => 'db_mysql','debug' => false,'pconnect' => 0,'autoconnect' => 0),
+);
